80187361849ee4aa5869fab10c3e4b5c1d34d6ef
Caio Lima <caiojlimah@gmail.com>
2023-12-19 09:31:35 -0300

    Updating backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 export const getMessages = async () => {
-    return fetch('https://rich-puce-bull-kilt.cyclic.app/message',{
+    return fetch('https://dark-erin-bandicoot-suit.cyclic.app/message',{
         method: 'GET'
     }).then((data) => data.json())
 }
 
 export const sendMessage = async (user, message) => {
     console.log(`Dentro da api: ${{user, message}}`);
-    return fetch('https://rich-puce-bull-kilt.cyclic.app/message',{
+    return fetch('https://dark-erin-bandicoot-suit.cyclic.app/message',{
         method: 'POST',
         headers: {
             'Accept': 'application/json',
