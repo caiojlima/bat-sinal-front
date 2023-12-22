e5f689451b39214e30fca2129c2e9bf96abf9440
Caio Lima <caiojlimah@gmail.com>
2023-12-21 22:32:13 -0300

    updating backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 export const getMessages = async () => {
-    return fetch('https://dark-erin-bandicoot-suit.cyclic.app/message',{
+    return fetch('https://bat-sinal.vercel.app/message',{
         method: 'GET'
     }).then((data) => data.json())
 }
 
 export const sendMessage = async (user, message) => {
     console.log(`Dentro da api: ${{user, message}}`);
-    return fetch('https://dark-erin-bandicoot-suit.cyclic.app/message',{
+    return fetch('https://bat-sinal.vercel.app/message',{
         method: 'POST',
         headers: {
             'Accept': 'application/json',
