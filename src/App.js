d88ce362558611f1e2dcf564be50eef2c7b10ead
Caio Lima <caiojlimah@gmail.com>
2023-12-22 01:58:47 -0300

    Adjusting css

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
   const updateState = (user, message) => {
     if (message) sendMessage(user, message)
     
-    setState([{ user, message: input.message }, ...state])
     setInput({...input, message: ''})
   }
 
